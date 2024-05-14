0a648d662b99b576259889f08c3d14c071397e40
Hitesh060201 <hiteshmishra145@gmail.com>
2024-05-15 02:08:17 +0530

    git committ

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require('express')
 const cors = require('cors')
 const dotenv =require('dotenv');
+const path =require('path')
 
 
 //dotenv configuration
@@ -14,10 +15,17 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+//static files access
+app.use(express.static(path.join(__dirname, './client/build')))
 
 
 //routes
-app.use("/api/v1/portfolio",require("./routes/PortfolioRoute"))
+app.use("/api/v1/portfolio",require("./routes/PortfolioRoute"));
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
+
 
 
 //port
